47c3e45c8f0b6e03a5d73f34b1b0528e1d342539
Emanuel MUstea <plus.emanuelmustea@gmail.com>
2020-03-21 13:33:35 -0700

    debugging user on prod

--- src/authentication/AuthenticationController.ts
+++ src/authentication/AuthenticationController.ts
@@ -25,17 +25,26 @@ export class AuthenticationController {
     @RequestMethod("post")
     @ValidateBody(AuthenticationSchema)
     public async authenticate({ email, password }: AuthenticationCredentials): Promise<AuthenticationPayload> {
+        const befgu = new Date().getTime();
+        console.log("before get user", befgu);
         const user = await this.userDb.getUserByEmail(email);
+        const aftgu = new Date().getTime();
+        console.log("after get user", aftgu, aftgu - befgu);
         const passwordMatches = await this.encryptionService.verify(password, user.password);
         if (!passwordMatches) {
             throw this.errorService.getErrorMessage(20);
         }
+        const aftpm = new Date().getTime();
+        console.log("after password matches", aftpm, aftpm - aftgu);
         const tokenPayload: TokenPayload = {
             id: user.id
         };
         const token = this.authenticationService.signToken(tokenPayload);
+        const afts = new Date().getTime();
+        console.log("after sign token", afts, afts - aftpm);
         const cleanedUser = this.utilsService.cleanUnsafeValues(user, UserSchema);
-
+        const afcu = new Date().getTime();
+        console.log("after cleaning user", afcu, afcu - afts);
         return {
             token,
             user: cleanedUser
